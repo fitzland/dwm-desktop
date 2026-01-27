70346b582fbaf37e55c3e6452050aed643c83dcb
fitzland <f1tzl4nd@gmail.com>
2026-01-27 07:00:27 -0600

    edit deploy config script

--- deploy-config.sh
+++ deploy-config.sh
@@ -18,8 +18,8 @@ mkdir -p "$TARGET_DIR"
 # --delete = remove files in target that no longer exist in source (keeps target clean)
 # --dry-run = test first (uncomment to preview)
 
-rsync -avh --progress --delete --dry-run \
-#rsync -avh --progress --delete \
+#rsync -avh --progress --delete --dry-run \
+rsync -avh --progress --delete \
   --exclude '.gitignore' \
   "$SOURCE_DIR/sxhkd/"     "$TARGET_DIR/sxhkd/" \
   "$SOURCE_DIR/dunst/"     "$TARGET_DIR/dunst/" \
